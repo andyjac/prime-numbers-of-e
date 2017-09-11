c536c5e195874b57716a52c069ce70f4b4cd8019
Andrew Jacobson <andrew.isaac.jacobson@gmail.com>
2017-09-10 21:23:45 -0700

    Fix typo

--- src/components/Info.js
+++ src/components/Info.js
@@ -8,7 +8,7 @@ const Info = (props) => {
         This tool lets you to calculate the <strong>Xth</strong> prime number that has <strong>Y</strong> digits in the
         expansion of <a href='https://en.wikipedia.org/wiki/E_(mathematical_constant)'><strong><em>Euler's Number</em></strong></a>.
         Enter two values below and hit <strong>Calculate</strong>, <strong><em>OR</em></strong> upload a formatted .csv file
-        where each line containes two comma separated numerical values for the <strong>occurrence</strong> and <strong>number of digits</strong> respectively.
+        where each line contains two comma separated numerical values for the <strong>occurrence</strong> and <strong>number of digits</strong> respectively.
       </p>
     </section>
   );
