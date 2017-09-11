f3e05ae9c1078b9d1968b3f847f364af1f746ad3
Andrew Jacobson <andrew.isaac.jacobson@gmail.com>
2017-09-10 17:37:20 -0700

    Include new components in App.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import Info from './components/Info';
+import PrimeCalculator from './components/PrimeCalculator';
 
 class App extends Component {
   render() {
-    return <h1>Hello World!</h1>;
+    return (
+      <section id='app-container'>
+        <div className='content'>
+          <Info/>
+          <PrimeCalculator/>
+        </div>
+      </section>
+    );
   }
 }
 
